4e6ee08b0091e99f536ba209a154269c0b888ceb
chautob0t <chautob0t@outlook.com>
2015-02-15 02:19:27 +0530

    Json_Deserialize
    Sample app to show Json Deserialization using Newtonsoft Json.NET
    library

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using JSON_Deserialize.Resources;
+
+namespace JSON_Deserialize
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using JSON_Deserialize.Resources;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace JSON_Deserialize
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        public class Mydata
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string location { get; set; }
+            public string date { get; set; }
+            public string time { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Mydata> mydata { get; set; }
+        }
+
+        // Reading content of file in a string
+        public static string ReadFile(string filePath)
+        {
+            var ResrouceStream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative));
+            if (ResrouceStream != null)
+            {
+                Stream myFileStream = ResrouceStream.Stream;
+                if (myFileStream.CanRead)
+                {
+                    StreamReader myStreamReader = new StreamReader(myFileStream);
+                    return myStreamReader.ReadToEnd();
+                }
+            }
+            return "";
+        }
+
+        private void Click_to_Deserialize_Click(object sender, RoutedEventArgs e)
+        {
+            var sample = ReadFile(@"Assets/samplejson.txt");
+
+            RootObject obj = JsonConvert.DeserializeObject<RootObject>(sample);
+
+            //MessageBox.Show(sample);
+            //test.DataContext = obj;
+
+            MessageBox.Show("Deserialization Succesfull!");
+        }
+
+
+    }
+}
